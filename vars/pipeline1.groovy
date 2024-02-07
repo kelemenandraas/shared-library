4396961d05f18f71a7eead8efdbf7a3756774aca
kelemenandraas <andras.kelemen@dbx.hu>
2024-02-07 17:24:23 +0100

    Test ErsteLibs polling time

--- vars/pipeline1.groovy
+++ vars/pipeline1.groovy
@@ -1,7 +1,20 @@
 def call() {
     pipeline {
-        agent any
+        agent {
+            node {
+                label 'master-node'
+                customWorkspace "c:\\Program Files\\Jenkins Workspace\\"
+            }
+        }   
         stages {
+        stage('Checkout') {
+			steps {
+				cleanWs()
+			    
+				checkout scmGit(branches: [[name: 'project/BSEFixAdapter']], extensions: [], userRemoteConfigs: [[credentialsId: '9dc4f8ab-22d0-4a64-83d7-986bd68e4b86', url: 'https://github.com/dbx/ErsteLibs.git']])
+				
+			}
+		}
             stage('Build') {
                 steps {
                     echo 'Building...'
